5e3203cce67fa2f32f44cb9e7106e42c8a3739b6
大鲨鱼爱吃皮皮 <46467974+MisterZongqiang@users.noreply.github.com>
2024-03-14 19:18:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,7 +13,7 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
 
-  USE_PASSAGE: 'push-deer',
+  USE_PASSAGE: 'server-chan',
   
   PROVINCE: '江苏',
   CITY: '南京',
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU28835TloCEtabodZ0WqPAQefk0corbxdjWzoPv',
+      id: 'SCT242198Tnv1pNcR0ebS69VWTVV9QcaVx',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -56,7 +56,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '自己',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU28835TloCEtabodZ0WqPAQefk0corbxdjWzoPv',
+      id: 'SCT242198Tnv1pNcR0ebS69VWTVV9QcaVx',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0003',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -89,7 +89,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU28835TloCEtabodZ0WqPAQefk0corbxdjWzoPv',
+      id: 'SCT242198Tnv1pNcR0ebS69VWTVV9QcaVx',
     }
   ],
 
